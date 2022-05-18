4c3f5d92197d052bb6457199dc1d7874145a7b27
Alexey Abramenko <a.abramenko@chita.ru>
2022-05-19 00:06:48 +0900

    Add. New Path for getting posts data

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ use abramenko\template;
 $router = new Router ();
 $router->addPath ('default', "indexPage");
 $router->addPath ('/admin/', "administratorPage");
+$router->addPath ('/get/', "getPosts");
 $router->run ();
 
 exit;
@@ -25,4 +26,37 @@ function indexPage ()
 function administratorPage ()
 {
     echo "You selected Admin Section<br />";
+}
+
+function getPosts ()
+{
+    $db = new DB ();
+    $results = $db->Query ('select id, upid, name, text from posts order by id');
+    if ($results) {
+        $results = collapseTree ($results);
+    } else {
+        $results = (object) [
+            'error' => $db->errorsList ()
+        ];
+    }
+    $template = new Template ();
+    $template->show ($results);
+}
+
+function collapseTree ($tree)
+{
+    $result = [];
+    foreach ($tree as $item) {
+        if ($item->upid != 0) {
+            if (!empty ($result[$item->upid])) {
+                if (empty ($result[$item->upid]->childs)) {
+                    $result[$item->upid]->childs = [];
+                }
+                $result[$item->upid]->childs[] = $item;                
+            }
+        } else {
+            $result[$item->id] = (object) $item;
+        }
+    }
+    return $result;
 }
