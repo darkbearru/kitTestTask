03fa84d449ec91c405569e97272cc5383d41f506
Alexey Abramenko <a.abramenko@chita.ru>
2022-05-19 17:31:07 +0900

    Update. Добавление авторизации в Admin часть и мелкие правки шаблонизатора

--- libs/abramenko/controller.php
+++ libs/abramenko/controller.php
@@ -13,7 +13,7 @@ class Controller
 
         $router = new Router ($this);
         $router->addPath ('default', "pageIndex");
-        $router->addPath ('/admin/', "administratorPage");
+        $router->addPath ('/admin/', "pageAdmin");
         $router->addPath ('/api/', "pageAPI");
         $router->run ();
     }
@@ -21,20 +21,44 @@ class Controller
     public function pageIndex ()
     {
         $template = new Template ();
-        $template->show (["hello", "answer"], 'index.html');
+        $template->show (
+            [
+                "title" => "Тестовое задание",
+                "caption" => "Тестовое задание для компании «КИТ»",
+                "data-list" => []
+            ], 
+            'index.html'
+        );
+    }
+
+    public function pageAdmin ()
+    {
+        $data = [
+            "title" => "Администрирование",
+            "caption" => "Тестовое задание для компании «КИТ». Администрирование",
+            "data-list" => [],
+            "login-form" => []
+        ];
+        $this->_auth = new Authorization ();
+        
+        $this->checkLoginLogout ();
+
+        if ($this->_auth->isLogined ()) {
+            $data["is-logined"] =  (object) ["show" => true];
+            $data["login-form"] = [];
+        } else {
+            $data["is-logined"] = [];
+            $data["login-form"] = (object) ["show" => true];
+        }
+
+        $template = new Template ();
+        $template->show ($data, 'admin.html');
     }
 
     public function pageAPI ()
     {
-        $db = new DB ();
-        $results = $db->Query ('select id, upid, name, text from posts order by id');
-        if ($results) {
-            $results = $this->collapseTree ($results);
-        } else {
-            $results = (object) [
-                'error' => $db->errorsList ()
-            ];
-        }
+        $results = $this->getTree ();
+
         $template = new Template ();
         $template->show ($results);
     }
@@ -55,5 +79,33 @@ class Controller
             }
         }
         return $result;
-    }    
+    }
+
+    private function checkLoginLogout ()
+    {
+        if (!$this->_auth->isLogined () && $this->_method === "POST") {
+            if (!empty ($_POST['login']) && !empty ($_POST['password'])) {
+                $_user = addslashes ($_POST['login']);
+                $_password = addslashes ($_POST['password']);
+                $this->_auth->Login ($_user, $_password);
+            }
+        } else if (!empty ($_POST['logout'])) {
+            $this->_auth->Logout ();
+        }
+    }
+
+    function getTree ()
+    {
+        $db = new DB ();
+        $results = $db->Query ('select id, upid, name, text from posts order by id');
+        if ($results) {
+            $results = $this->collapseTree ($results);
+        } else {
+            $results = (object) [
+                'error' => $db->errorsList ()
+            ];
+        }
+        return $results;
+    }
+    
 }
--- libs/abramenko/template.php
+++ libs/abramenko/template.php
@@ -32,13 +32,51 @@ class Template
     private function returnHTML ($data, $templateFile)
     {
         $this->makeHeader ();
-        $_file = implode ('', file ($templateFile));
-        echo $_file;
+        $file = implode ('', file ($templateFile));
+        if (!empty ($data)) {
+            $file = $this->placeVariables ($data, $file);
+        }
+        $file = $this->clearNotSet ($file);
+        echo $file;
         exit;
     }
 
     private function makeHeader ($isJSON = false) {
         header ('Content-type: '.($isJSON ? 'application/json' : 'text/html').'; charset=utf-8');
         header ('Last-Modified: '.date ('Y-m-d H:i:s'));
+    }
+
+    private function placeVariables ($variables, $html)
+    {
+        $variables = (array) $variables;
+        foreach ($variables as $key => $value) {
+            if (is_string ($value) || is_numeric ($value)) {
+                $html = preg_replace("/\{\{${key}\}\}/uim", $value, $html);
+            } else {
+                if (is_array ($value) || is_object ($value)) {
+                    $value = (array) $value;
+                    if (empty ($value)) {
+                        $html = preg_replace ("/\{\{(${key})\}\}(.*?)\{\{\/${key}\}\}/uis", '', $html);
+                    } else if (preg_match_all ("/\{\{(${key})\}\}(.*?)\{\{\/${key}\}\}/uis", $html, $matches, PREG_SET_ORDER)){
+
+                        $_html = '';
+                        foreach ($matches as $matchStr) {
+                            foreach ($value as $_key => $_val) {
+                                if (!empty ($matchStr[2])) {
+                                    $_html .= $this->placeVariables ([$_key => $_val], $matchStr[2]);
+                                }
+                            }
+                        }
+                        preg_replace ("/\{\{(${key})\}\}(.*?)\{\{\/${key}\}\}/uis", $_html, $html);
+                    }
+                }
+            }
+        }
+        return $html;
+    }
+
+    private function clearNotSet ($html)
+    {
+        return preg_replace('/\{\{(\/?[\w\-]{2,})\}\}/uis', '', $html); 
     }
 }
