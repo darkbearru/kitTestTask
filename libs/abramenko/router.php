d90b5fa8f78b9b3b7093d34bdfc111a847d2aac9
Alexey Abramenko <a.abramenko@chita.ru>
2022-05-29 21:02:17 +0900

    Fix. Включение редиректа в настройках Nginx, а также разрешение методов PUT & DELETE. И корректировка Route в соответствии со стандартами REST API

--- libs/abramenko/router.php
+++ libs/abramenko/router.php
@@ -2,42 +2,110 @@
 
 namespace abramenko;
 
+/**
+ * class Router
+ * 
+ * Реализация REST API
+ */
 class Router 
 {
     private $_routerPaths;
     private $_parent;
+    private $_default;
 
-    public function __construct ($parent) 
+    /**
+     * Конструктор
+     * Передаётся, родительский класс чьи методы вызываются
+     * А также метод вызываемый по умолчанию
+     */
+    public function __construct ($parent, $default = false) 
     {
         $this->_routerPaths = [];
+        if (!is_object ($parent)) return false;
+        
         $this->_parent = $parent;
+
+        if (!empty ($default)) {
+            if (method_exists ($parent, $default)) {
+                $this->_default = $default;
+            }
+        }
     }
 
-    public function addPath ($path, $action)
+
+    /**
+     * Метод PUT,
+     * Используется для изменения существующих данных
+     */
+    public function put ($path, $action) 
     {
-        $this->_routerPaths[$path] = $action;
+        $this->addPath ('PUT', $path, $action);
     }
 
+    /**
+     * Метод GET
+     * Используется для получаения каких либо данных
+     */
+    public function get ($path, $action) 
+    {
+        $this->addPath ('GET', $path, $action);
+    }
+
+    /**
+     * Метод POST
+     * Используется для создания / внесения данных
+     */
+    public function post ($path, $action) 
+    {
+        $this->addPath ('POST', $path, $action);
+    }
+
+    /**
+     * Метод Delete
+     * Используется для удаления данных
+     */
+    public function delete ($path, $action) 
+    {
+        $this->addPath ('DELETE', $path, $action);
+    }
+
+    /**
+     * Запускаем роутинг и вызываем в случае необходимости нужный метод
+     */
     public function run ()
     {
-        $_url = !empty ($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '/';
-        $_params = explode ('?', $_url);
-        $_url = $_params[0];
+        $method = !empty ($_SERVER["REQUEST_METHOD"]) ? $_SERVER["REQUEST_METHOD"] : 'GET';
+        $url = !empty ($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '/';
+        $params = explode ('?', $url);
+        $url = $params[0];
+        $params = count ($params) > 1 ? explode ('&', $params[1]) : [];
 
-        if (!empty ($this->_routerPaths[$_url])) {
-            $_action = $this->_routerPaths[$_url];
-            if (method_exists ($this->_parent, $_action)) {
-                $this->_parent->$_action ();
-                return true;
-            }
-        }
-        // If nothing found, show default page
-        $_action = $this->_routerPaths["default"];
-        if (!empty ($_action)) {
-            if (method_exists ($this->_parent, $_action)) {
-                $this->_parent->$_action ();
-                return true;
-            }
-        }
+        $result = $this->processPath ($method, $url, $params);
+        if ($result) return $result;
+
+        return $this->_parent->{$this->_default} ($params);
+    }
+
+    /**
+     * Добавляем нужный путь, обработки необходимого метода, а также наименование своего метода
+     */
+    protected function addPath ($method, $path, $action)
+    {
+        if (!empty ($this->_routerPaths[$method])) $this->_routerPaths[$method] = [];
+
+        $this->_routerPaths[$method][$path] = $action;
+    }
+
+    /**
+     * Вызов нужного метода для необходимого пути
+     */
+    protected function processPath ($method, $url, $params) 
+    {
+        if (empty ($this->_routerPaths[$method])) return false;
+        if (empty ($this->_routerPaths[$method][$url])) return false;
+
+        $action = $this->_routerPaths[$method][$url];
+
+        return  $this->_parent->{$action} ($params);
     }
 }
